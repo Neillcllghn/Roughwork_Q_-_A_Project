013275c0f8c5c7d672aeb7bda6ae8ae3256d3369
Neillcllghn <neillcllghn@outlook.com>
2022-10-22 14:24:24 +0000

    intial Javascript up to min 48.10

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,3 +3,104 @@ const choices = Array.from(document.querySelectorAll('.choice-text'));
 const ProgressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
+    {
+        question: 'What is 2 + 2',
+        choice1: '2',
+        choice2: '4',
+        choice3: '21',
+        choice4: '17',
+        answer: 2,
+    },
+    {
+        question: 'Who was the President of the US at the beginning of the 2nd World War',
+        choice1: 'Franklin D. Roosevelt',
+        choice2: 'Ronald Reagan4',
+        choice3: 'Geogre Bush Snr',
+        choice4: 'JFK',
+        answer: 1,
+    },
+    {
+        question: 'Who is the top scorer in Premier League History',
+        choice1: 'Sergio Aguero',
+        choice2: 'Mohammad Salah',
+        choice3: 'Alan Shearer',
+        choice4: 'Thierry Henry',
+        answer: 3,
+    },
+    {
+        question: 'What is the captial city of France',
+        choice1: 'Washington DC',
+        choice2: 'Dublin',
+        choice3: 'London',
+        choice4: 'Paris',
+        answer: 4,
+    }
+
+]
+
+const SCORE_POINTS = 100;
+const MAX_QUESTIONS = 4;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion()
+}
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionsCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('/end.html')
+    }
+
+    questionCounter++
+    ProgressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+
+    const questionsIndex = Math.floor(math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number'];
+        choice.innertext = currentQuestion['choice' + number]
+    })
+
+    availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+        if(classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+        
+        }, 1000)
+    })
+})
